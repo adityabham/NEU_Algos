6c616118e2f89843a1143be4d8b791d0019d402b
Aditya Bhamidipati <adityabhamidipati@macbook-pro.myfiosgateway.com>
2021-03-21 19:13:14 -0400

    Noise update

---
+++ EECE3468/mat3/part_i_ii.m
@@ -0,0 +1,59 @@
+clc; 
+clear;
+N=1000; % length of the array of random variables 
+
+% X1=Uniform(2,4)
+
+% a=2; b=4; 
+% pd_x1 = makedist('uniform','lower',a,'upper',b); 
+% X1=random(pd_x1,N,1);
+% X1_sort=sort(X1); 
+% fX1=pdf(pd_x1,X1_sort); 
+% figure(1)
+% plot(X1_sort,fX1,'Color','r', 'LineWidth', 2);
+% FX1=cdf(pd_x1,X1_sort);
+% figure(2)
+% plot(X1_sort,FX1,'Color','r', 'LineWidth', 2); 
+% 
+% calc_mean_1 = mean(X1_sort); 
+% calc_var_1 = var(X1_sort); 
+% theo_mean_1 = (b+a)/2;
+% theo_var_1 = (b-a)^2/12;
+
+% X2=Gaussian(Normal)(mu=2,sigma=1) 
+
+% pd_x2 = makedist('Normal','mu',2,'sigma',1); 
+% X2=random(pd_x2,N,1);
+% X2_sort=sort(X2);
+% fX2=pdf(pd_x2,X2_sort); 
+% figure(1)
+% plot(X2_sort,fX2,'Color','r', 'LineWidth', 2);
+% FX2=cdf(pd_x2,X2_sort);
+% figure(2)
+% plot(X2_sort,FX2,'Color','r', 'LineWidth', 2); 
+
+% calc_mean_2 = mean(X2_sort); 
+% calc_var_2 = var(X2_sort); 
+% theo_mean_2 = 2;
+% theo_var_2 = 1;
+
+% X3=Exponential(lambda=0.5)
+% lambda = 0.5; 
+% pd_x3 = makedist('Exponential','mu',1/lambda); 
+% X3=random(pd_x3,N,1);
+% X3_sort=sort(X3);
+% fX3=pdf(pd_x3,X3_sort); 
+% figure(1)
+% plot(X3_sort,fX3,'Color','r', 'LineWidth', 2);
+% FX3=cdf(pd_x3,X3_sort);
+% figure(2)
+% plot(X3_sort,FX3,'Color','r', 'LineWidth', 2); 
+% 
+% calc_mean_3 = mean(X3_sort); 
+% calc_var_3 = var(X3_sort); 
+% theo_mean_3 = 1/lambda;
+% theo_var_3 = 1/(lambda)^2;
+
+
+
+
---
+++ EECE3468/mat3/part_iii.m
@@ -0,0 +1,28 @@
+clc; 
+clear;
+N=10000; % length of the array of random variables 
+
+% Y1=2*X1-1
+% X1=Uniform(2,4)
+
+a=2; b=4; 
+pd_x1 = makedist('uniform','lower',a,'upper',b); 
+X1=random(pd_x1,N,1);
+Y1 = 2*X1-1; 
+
+calc_mean_1 = mean(Y1); 
+calc_var_1 = var(Y1); 
+theo_mean_1 = 2*((b+a)/2)-1;
+theo_var_1 = 4*((b-a)^2/12);
+
+% Y2=2*X2
+% X2=Gaussian(Normal)(mu=2,sigma=1)
+
+pd_x2 = makedist('Normal','mu',2,'sigma',1); 
+X2=random(pd_x2,N,1);
+Y2 = 2*X2; 
+
+calc_mean_2 = mean(Y2); 
+calc_var_2 = var(Y2); 
+theo_mean_2 = 4;
+theo_var_2 = 4;
