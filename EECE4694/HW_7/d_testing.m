35715fce55b088f0af09dbe5ccfed42012423d7c
Aditya Bhamidipati <adityabhamidipati@MacBook-Pro.local>
2021-03-28 22:36:48 -0400

    HW7 Fin

--- EECE4694/HW_7/d_testing.m
+++ EECE4694/HW_7/d_testing.m
@@ -18,10 +18,10 @@ z = log(Id);
 p = polyfit(v2,z,1); 
 Is = exp(p(2));
 Vt = 1/p(1); 
-yfit = Is*exp(v2/Vt); 
+Id_fit = Is*exp(v2/Vt); 
 plot(v2,Id,'*'); grid on;
 hold on;
-plot(v2,yfit)
+plot(v2,Id_fit)
 
 % part c 
 k = 1.38e-23; 
@@ -33,4 +33,16 @@ disp(T_calc);
 nVt = 1/p(1);
 n = (q*nVt)/(k*300); 
 disp(n) 
+
+% part e 
+St = sum((Id-mean(Id)).^2);
+Sr = sum((Id-Id_fit).^2); 
+r2 = (St-Sr)/St;
+disp(r2)      
+
+% part f 
+vD_eval = [0.375, 0.4, 0.425, 0.45, 0.628, 0.64, 0.653, 0.662]; 
+Id_new = spline(v2,Id,vD_eval); 
+plot(vD_eval,Id_new,'ro');
+legend('raw data','fitted curve','interpolated points','Location','southeast')
 
